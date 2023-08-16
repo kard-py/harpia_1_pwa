b4b8fe23b6fc27c448ea48b7f54f3722cb7ebdb8
CaioDetz <caiodetz654@gmail.com>
2023-08-16 18:31:46 +0000

    UP

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -14,7 +14,7 @@ interface Props {
 const Root = ({ children }: Props) => {
   return (
     <div className="w-full h-full flex flex-row">
-      <SideBar.root className="md:w-64 w-0 h-screen gap-5 bg-black text-white flex flex-col items-center pt-5">
+      <SideBar.root className="md:w-64 w-0 h-screen overflow-y-scroll gap-5 bg-black text-white flex flex-col items-center pt-5">
         <SideBar.option href={"/"}>
           <SideBar.icon src={home} alt="Home" />
           <SideBar.label className="text-white">Home</SideBar.label>
