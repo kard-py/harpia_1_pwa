eb823146d6a269546bf339eaaa76ffd97b2c8f39
Caio Detz <89801456+kard-py@users.noreply.github.com>
2023-08-25 12:10:37 +0000

    UP

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins");
+const withPwa = require("next-pwa")({
+  dest: "public",
+  register: true,
+  sw: "/sw.js",
+  disable: process.env.NODE_ENV != "production",
+});
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPwa({
+  experimental: {
+    serverActions: true,
+  },
+});
